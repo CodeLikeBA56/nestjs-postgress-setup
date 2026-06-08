8805853d735ad0d55b09752f31840f75ec79b670
Sameer Shamshad <sameer.shamshad.dev@gmail.com>
2026-06-08 20:47:03 +0500

    feat: add Swagger documentation setup for API

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -12,6 +13,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Project Management Server API Documentation.')
+    .setDescription('The API documentation of project management server.')
+    .setVersion('1.0')
+    .addTag('swagger-example')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
