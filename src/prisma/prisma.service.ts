f042710c6f0cbe642023c318a221dcb731855f6f
Sameer Shamshad <sameer.shamshad.dev@gmail.com>
2026-06-11 17:06:10 +0500

    fix: update error handling in PrismaService and improve docker-compose formatting

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,11 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
-import { BadRequestException, Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
@@ -8,7 +13,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
     const databaseUrl = process.env.DATABASE_URL;
 
     if (!databaseUrl) {
-      throw new BadRequestException('The Postgress database url is missing.');
+      throw new InternalServerErrorException('The Postgress database url is missing.');
     }
 
     const adapter = new PrismaPg({
